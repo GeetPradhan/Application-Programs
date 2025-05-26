fd9cbb678010fc2150ea2830754b753834cc5882
GeetPradhan <132755081+GeetPradhan@users.noreply.github.com>
2025-05-26 19:23:52 +0530

    Add files via upload

---
+++ program101.c
@@ -0,0 +1,34 @@
+// Input : 7
+// Output : 7   *   6   *   5   *   4   *   3   *   2   *   1   *
+
+// Input : 3
+// Output : 3   *   2   *   1   *   
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo; iCnt >= 1; iCnt--)
+    {
+        printf("%d\t*\t",iCnt);
+    }
+    printf("\n");
+    int i = 0;
+}
+
+int main()
+{
+    int iFrequency = 0;
+
+    printf("Enter the frequency of symbol : \n");
+    scanf("%d",&iFrequency);
+
+    Display(iFrequency);
+
+    return 0;
+}
+
+
+//      7   *   6   *   5   *   4   *   3   *   2   *   1   *
